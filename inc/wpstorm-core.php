f2ff0caa890482cbfc5a4ef53cf204b92c4eabef
esradev <wpstormdev@gmail.com>
2025-02-17 16:42:17 +0330

    Feat: Update theme description and enhance menu settings with localization and submenu options

--- inc/wpstorm-core.php
+++ inc/wpstorm-core.php
@@ -58,16 +58,42 @@ class Wpstorm_Core
 
     public function add_theme_menu()
     {
-        add_menu_page('Wpstorm Theme Settings', 'Wpstorm Theme Settings', 'manage_options', 'wpstorm-theme-settings', [$this, 'settings_page']);
+        add_menu_page(
+            __('Wpstorm Theme', 'wpstorm-theme'), 
+            __('Wpstorm Theme', 'wpstorm-theme'),
+            'manage_options',
+            'wpstorm-theme-settings',
+            [$this, 'settings_page'],
+            'data:image/svg+xml;base64,' . base64_encode('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-paintbrush-vertical"><path d="M10 2v2"/><path d="M14 2v4"/><path d="M17 2a1 1 0 0 1 1 1v9H6V3a1 1 0 0 1 1-1z"/><path d="M6 12a1 1 0 0 0-1 1v1a2 2 0 0 0 2 2h2a1 1 0 0 1 1 1v2.9a2 2 0 1 0 4 0V17a1 1 0 0 1 1-1h2a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1"/></svg>'),
+            2
+        );
+        add_submenu_page(
+            'wpstorm-theme-settings',
+            __('Settings', 'wpstorm-theme'),
+            __('Settings', 'wpstorm-theme'),
+            'manage_options',
+            'wpstorm-theme-settings',
+            [$this, 'settings_page']
+        );
+        add_submenu_page(
+            'wpstorm-theme-settings',
+            __('Login', 'wpstorm-theme'),
+            __('Login', 'wpstorm-theme'),
+            'manage_options',
+            '#/login',
+        );
     }
 
     public function settings_page()
     {
         ?>
-
-<div id="wpstorm-theme-settings-dashboard" class="wpstorm-theme-tw-class"></div>
-
-<?php
+            <div id="wpstorm-theme-settings-dashboard" class="wpstorm-theme-tw-class"></div>
+            <style type="text/css">
+                .notice {
+                    display: none;
+                } 
+            </style>
+        <?php
     }
 
 
